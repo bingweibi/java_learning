fc0003bb6246582dadf879975579ee6de92249be
bingweibi <794654932@qq.com>
2017-03-07 17:10:12 +0800

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("程序员的第一个程序，helloworld");
+        System.out.println("Hello World!");
+    }
+}
